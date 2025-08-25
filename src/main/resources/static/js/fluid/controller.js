f30c1a789b200434d876061857e20a6b09485f7d
Reed Evertt <reed@evertt.com>
2025-08-25 12:26:13 -0700

    Drafted first fluid simulation test

--- src/main/resources/static/js/fluid/controller.js
+++ src/main/resources/static/js/fluid/controller.js
@@ -13,18 +13,36 @@ const DEBUG_VERBOSITY = 1;
 var canvas;
 var gl;
 var shaders = {
-    program: null,
-    vert: null,
-    frag: null,
-    attributeLocs: null,
-    uniformLocs: null,
-    vertexBuffer: null,
+    sim: {
+        program: null,
+        vert: null,
+        frag: null,
+        attributeLocs: null,
+        uniformLocs: null,
+    },
+    draw: {
+        program: null,
+        vert: null,
+        frag: null,
+        attributeLocs: null,
+        uniformLocs: null,
+    },
+    vertexbuffers: {
+        positions: null,
+        st: null,
+    },
+    simTex1: null,
+    simTex2: null,
+    simFB: null,
 }
 var enabled;
 var curCanvW;
 var curCanvH;
+var curSimW;
+var curSimH;
 var timeSim = -1; // ms it's been running
 var timePrev = new Date().getMilliseconds();
+var frameParity = 0;
 var lastTimeDelta;
 var focused = true;
 
@@ -45,16 +63,22 @@ function pollResizeCanvas() {
         curCanvW === undefined || curCanvH === undefined ||
         // Otherwise, only refresh when above a certain threshold
         (desiredW - curCanvW) / curCanvW > canvasResizeTolerance ||
-        (desiredH - curCanvH) / curCanvH > canvasResizeTolerance) {
+        (desiredH - curCanvH) / curCanvH > canvasResizeTolerance)
+    {
         refreshCanvas(desiredW, desiredH);
     }
 }
 function refreshCanvas(newWidth, newHeight) {
-    // Delete old texture buffers
-    /* Implement me! */
+    // Resize the simulation texture
+    createSimTextures(newWidth, newHeight);
 
-    // Make new texture buffers
-    /* Implement me! */
+    // Resize the canvas
+    gl.canvas.width = newWidth;
+    gl.canvas.height = newHeight;
+
+    // Update tracker values
+    curCanvW = newWidth;
+    curCanvH = newHeight;
 }
 function tryUpdateRepeating(_ = null) {
     timeCur = new Date().getMilliseconds();
@@ -72,13 +96,80 @@ function tryUpdateRepeating(_ = null) {
         requestAnimationFrame(tryUpdateRepeating);
 }
 function updateSim(deltaT) {
+    // Sim textures alternate between input & output
+    let texPrev  = frameParity === 0 ? shaders.simTex1 : shaders.simTex2;
+    let texNext = frameParity === 0 ? shaders.simTex2 : shaders.simTex1;
+    frameParity = frameParity === 0 ? 1 : 0;
+
     // Update the fluid simulation
-    /* Implement me! */
+    simStep(deltaT, texPrev, texNext);
 
     // Then render the changes
-    renderScene();
+    renderScene(texNext);
 }
-function renderScene() {
+function simStep(deltaT, texPrev, texNext) {
+    // We update the sim using the simulation fragment shaders
+
+    // Specify we render to framebuffer/next state texture
+    gl.bindFramebuffer(gl.FRAMEBUFFER, shaders.simFB);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texNext);
+    gl.bindTexture(gl.TEXTURE_2D, texNext);
+    gl.viewport(0, 0, curSimW, curSimH);
+
+    // Clear framebuffer
+    gl.clearColor(0, 0, 0, 1);
+    gl.clearDepth(1.);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // Specify program to render with
+    gl.useProgram(shaders.sim.program);
+
+    // Provide vertex buffer
+    gl.enableVertexAttribArray(shaders.sim.attributeLocs.vertexPosition);
+    gl.bindBuffer(gl.ARRAY_BUFFER, shaders.vertexbuffers.positions);
+    gl.vertexAttribPointer(
+        shaders.sim.attributeLocs.vertexPosition,
+        2, gl.FLOAT, false, 0, 0);
+
+    // Provide texture coord buffer
+    gl.enableVertexAttribArray(shaders.sim.attributeLocs.textureCoord);
+    gl.bindBuffer(gl.ARRAY_BUFFER, shaders.vertexbuffers.textureCoord);
+    gl.vertexAttribPointer(
+        shaders.sim.attributeLocs.textureCoord,
+        2, gl.FLOAT, false, 0, 0);
+
+    // Create projection matrix
+    let zClipNear = 0.1;
+    let zClipFar = 20.0;
+    let projMatrix = mat4.create();
+    mat4.ortho(projMatrix, -1.0, 1.0, -1.0, 1.0, zClipNear, zClipFar);
+    // Provide proj matrix
+    gl.uniformMatrix4fv(shaders.sim.uniformLocs.projectionMatrix, false, projMatrix);
+
+    // Create model view matrix
+    let modelViewMatrix = mat4.create();
+    mat4.translate(modelViewMatrix, modelViewMatrix, [0., 0., -1.]);
+    // Provide model view matrix
+    gl.uniformMatrix4fv(shaders.sim.uniformLocs.modelViewMatrix, false, modelViewMatrix);
+
+    // Provide the previous state texture as the input
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texPrev);
+    gl.uniform1i(shaders.sim.uniformLocs.textureSampler, 0);
+
+    // Draw 'em!
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+    if (DEBUG_VERBOSITY >= 1)
+        console.log(`Simulation updated with timestep ${deltaT}!`);
+}
+function renderScene(texNext) {
+    // Unbind framebuffer from simulation -- render to the canvas!
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
     // Clear all existing fragments
     gl.clearColor(0., 0., 0., 1.);
     gl.clearDepth(1.);
@@ -86,34 +177,41 @@ function renderScene() {
     gl.depthFunc(gl.LEQUAL);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+    // Specify program to render with
+    gl.useProgram(shaders.draw.program);
+
+    // Provide vertex buffer
+    gl.enableVertexAttribArray(shaders.draw.attributeLocs.vertexPosition);
+    gl.bindBuffer(gl.ARRAY_BUFFER, shaders.vertexbuffers.positions);
+    gl.vertexAttribPointer(
+        shaders.draw.attributeLocs.vertexPosition,
+        2, gl.FLOAT, false, 0, 0);
+
+    // Provide texture coord buffer
+    gl.enableVertexAttribArray(shaders.draw.attributeLocs.textureCoord);
+    gl.bindBuffer(gl.ARRAY_BUFFER, shaders.vertexbuffers.textureCoord);
+    gl.vertexAttribPointer(
+        shaders.draw.attributeLocs.textureCoord,
+        2, gl.FLOAT, false, 0, 0);
+
     // Create projection matrix
-    let aspect = curCanvW / curCanvH;
     let zClipNear = 0.1;
     let zClipFar = 20.0;
     let projMatrix = mat4.create();
     mat4.ortho(projMatrix, -1.0, 1.0, -1.0, 1.0, zClipNear, zClipFar);
+    // Provide proj matrix
+    gl.uniformMatrix4fv(shaders.draw.uniformLocs.projectionMatrix, false, projMatrix);
 
     // Create model view matrix
     let modelViewMatrix = mat4.create();
-    mat4.translate(modelViewMatrix, modelViewMatrix,
-        [0., 0., -1.]
-    );
-
-    // Provide vertex buffer
-    gl.bindBuffer(gl.ARRAY_BUFFER, shaders.vertexBuffer);
-    gl.vertexAttribPointer(
-        shaders.attributeLocs.vertexPosition,
-        2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(shaders.attributeLocs.vertexPosition);
-
-    // Specify program to render with
-    gl.useProgram(shaders.program);
-
-    // Provide proj matrix
-    gl.uniformMatrix4fv(shaders.uniformLocs.projectionMatrix, false, projMatrix);
-
+    mat4.translate(modelViewMatrix, modelViewMatrix, [0., 0., -1.]);
     // Provide model view matrix
-    gl.uniformMatrix4fv(shaders.uniformLocs.modelViewMatrix, false, modelViewMatrix);
+    gl.uniformMatrix4fv(shaders.draw.uniformLocs.modelViewMatrix, false, modelViewMatrix);
+
+    // Provide the newly generated sim state texture as input
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, texNew);
+    gl.uniform1i(shaders.draw.uniformLocs.textureSampler, 0);
 
     // Draw 'em!
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
@@ -127,7 +225,11 @@ function renderScene() {
 //////////////////////////////////////////////
 
 function init() {
-    enabled = initCanvas() && initGL() && initShaderProgram();
+    enabled =
+        initCanvas() &&
+        initGL() &&
+        initShaderPrograms() &&
+        initVertexBuffers();
     if (enabled) {
         pollResizeCanvas();
 
@@ -183,7 +285,7 @@ function initGL() {
         // cxt.fillRect(0, 0, canvas.offsetWidth, canvas.offsetHeight);
         return false;
     }
-    // Draw our basic plain color while we wait
+    // Draw a basic plain color while we wait
     gl.clearColor(0, 0, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT);
     if (DEBUG_VERBOSITY >= 1)
@@ -204,44 +306,129 @@ function loadShader(name, type, source) {
         console.log(`Shader "${name}" loaded and compiled`);
     return s;
 }
-function initShaderProgram() {
+function initShaderPrograms() {
+    createShaderProgram("Fluid Simulation", shaders.sim,
+        SHADERSTR_FLUID_SIM_VERT, SHADERSTR_FLUID_SIM_FRAG,
+        null, [["deltaTime", "uDeltaTime"], ["texWidth", "uTexWidth"], ["texHeight", "uTexHeight"]]);
+    createShaderProgram("Fluid Draw", shaders.draw,
+        SHADERSTR_FLUID_DRAW_VERT, SHADERSTR_FLUID_DRAW_FRAG);
+}
+function createShaderProgram(name, storage, vertSource, fragSource,
+    extraAttributes = null, extraUniforms = null)
+{
     // Setup & link shader
-    shaders.vert = loadShader("Fluid: Vertex", gl.VERTEX_SHADER, SHADERSTR_FLUID_VERT);
-    if (shaders.vert === null) return false;
-    shaders.frag = loadShader("Fluid: Fragment", gl.FRAGMENT_SHADER, SHADERSTR_FLUID_FRAG);
-    if (shaders.frag === null) return false;
-    shaders.program = gl.createProgram();
-    gl.attachShader(shaders.program, shaders.vert);
-    gl.attachShader(shaders.program, shaders.frag);
-    gl.linkProgram(shaders.program);
+    let vert = loadShader(`${name}: Vertex`, gl.VERTEX_SHADER, vertSource);
+    if (vert === null) return false;
+    let frag = loadShader(`${name}: Fragment`, gl.FRAGMENT_SHADER, fragSource);
+    if (frag === null) return false;
+    let program = gl.createProgram();
+    gl.attachShader(program, vert);
+    gl.attachShader(program, frag);
+    gl.linkProgram(program);
 
     // Crash & burn if linking failed
-    if (!gl.getProgramParameter(shaders.program, gl.LINK_STATUS)) {
-        console.error("Shader program initialization failed.\nLog: " + gl.getProgramInfoLog(shaders.program));
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        console.error(`${name} shader program initialization failed.\nLog: ${gl.getProgramInfoLog(program)}`);
         return false;
     }
 
     if (DEBUG_VERBOSITY >= 1)
-        console.log("Shader program linked");
+        console.log(`${name} shader program successfully linked`);
 
     // Save location of shader variable's we'll need to manage
-    shaders.attributeLocs = {
-        vertexPosition:     gl.getAttribLocation(shaders.program, "aVertexPosition"),
+    let attributeLocs = {
+        vertexPosition: gl.getAttribLocation(program, "aVertexPosition"),
+        textureCoord:   gl.getAttribLocation(program, "aTextureCoord"),
     };
-    shaders.uniformLocs = {
-        projectionMatrix:   gl.getUniformLocation(shaders.program, "uProjectionMatrix"),
-        modelViewMatrix:    gl.getUniformLocation(shaders.program, "uModelViewMatrix"),
+    if (extraAttributes !== null) {
+        extraAttributes.forEach(a => {
+            attributeLocs[a[0]] = gl.getAttribLocation(program, a[1]);
+        });
+    }
+    let uniformLocs = {
+        projectionMatrix: gl.getUniformLocation(program, "uProjectionMatrix"),
+        modelViewMatrix:  gl.getUniformLocation(program, "uModelViewMatrix"),
+        textureSampler:   gl.getUniformLocation(program, "uTex"),
     };
+    if (extraUniforms !== null) {
+        extraUniforms.forEach(u => {
+            uniformLocs[u[0]] = gl.getUniformLocation(program, u[1]);
+        });
+    }
 
+    // If everything succeeded, save it
+    storage.program = program;
+    storage.vert = vert;
+    storage.frag = frag;
+    storage.attributeLocs = attributeLocs;
+    storage.uniformLocs = uniformLocs;
+    return true;
+}
+function initVertexBuffers() {
+    shaders.vertexbuffers.positions =
+        createVertexBuffer("Positions", [1., 1., -1., 1., 1., -1., -1., -1.]);
+    shaders.vertexbuffers.textureCoord =
+        createVertexBuffer("Texture Coordinates", [0., 0., 1., 0., 1., 1., 0., 1.]);
+}
+function createVertexBuffer(name, data) {
     // Create vertex buffer for a very boring plane
-    shaders.vertexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, shaders.vertexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(
-        [1., 1., -1., 1., 1., -1., -1., -1.]),
+    let vb = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vb);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data),
         gl.STATIC_DRAW);
 
     if (DEBUG_VERBOSITY >= 1)
-        console.log("Vertex buffer created");
+        console.log(`Vertex buffer "${name}" successfully created`);
 
-    return true;
+    return vb;
+}
+function createSimTextures(resX, resY) {
+    // Delete existing textures
+    if (shaders.simTex1 !== null)
+        gl.deleteTexture(shaders.simTex1);
+    if (shaders.simTex2 !== null)
+        gl.deleteTexture(shaders.simTex2);
+
+    // Create new render textures to act as alternating simulation buffers
+    let a1 = gl.TEXTURE_2D; // target
+    let a2 = 0; // mipmap level
+    let a3 = gl.RGBA; // internalFormat
+    let a4 = rexX; // width
+    let a5 = resY; // height
+    let a6 = 0; // border
+    let a7 = gl.RGBA; // srcFormat
+    let a8 = gl.UNSIGNED_BYTE; // srcType
+    let a9 = canvas; // pixel source (just copy what's already drawn on the canvas)
+    // Tex1
+    shaders.simTex1 = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, shaders.simTex1);
+    gl.texImage2D(a1, a2, a3, a4, a5, a6, a7, a8, a9);
+    // Wrap textures, because that'll look cool
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+    // Turn on filtering, but no mipmaps!
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    // Tex2
+    shaders.simTex2 = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, shaders.simTex2);
+    gl.texImage2D(a1, a2, a3, a4, a5, a6, a7, a8, a9);
+    // Wrap texture, because that'll look cool
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+    // Turn on filtering, but no mipmaps!
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+    // Create the framebuffer to help in rendering to the texture
+    // (only create it if one not already created)
+    if (shaders.simFB === null)
+        shaders.simFB = gl.createFramebuffer();
+
+    // Save values
+    curSimW = resX;
+    curSimH = resY;
+
+    if (DEBUG_VERBOSITY >= 1)
+        console.log(`Simulation texture/framebuffer of resolution ${resX}x${resY} created`);
 }
--- src/main/resources/static/js/fluid/shaderSources.js
+++ src/main/resources/static/js/fluid/shaderSources.js
@@ -1,23 +1,81 @@
 /////////////////////////////////////////////
 /*          ~~~ Vertex Shader ~~~          */
 /////////////////////////////////////////////
-const SHADERSTR_FLUID_VERT = `
+const SHADERSTR_FLUID_SIM_VERT = `
 attribute vec4 aVertexPosition;
+attribute vec4 aTextureCoord;
+
 uniform mat4 uModelViewMatrix;
 uniform mat4 uProjectionMatrix;
 
+varying highp vec2 vST;
+
 void main() {
     gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    vST = aTextureCoord;
+}
+`;
+
+const SHADERSTR_FLUID_DRAW_VERT = `
+attribute vec4 aVertexPosition;
+attribute vec4 aTextureCoord;
+
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+
+varying highp vec2 vST;
+
+void main() {
+    gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    vST = aTextureCoord;
 }
 `;
 
 ///////////////////////////////////////////////
 /*          ~~~ Fragment Shader ~~~          */
 ///////////////////////////////////////////////
-const SHADERSTR_FLUID_FRAG = `
-const mediump vec4 BASE_COL = vec4(144.0 / 255.0, 58.0 / 255.0, 214.0 / 255.0, 1.0);
+const SHADERSTR_FLUID_SIM_FRAG = `
+varying highp vec2 vST;
+
+uniform sampler2D uTex;
+uniform uint uTexWidth;
+uniform uint uTexHeight;
+uniform highp float uDeltaTime;
+
+const highp float BRIGHTNESS_TRANSITION_TIME = 8.0;
 
 void main() {
-    gl_FragColor = BASE_COL;
+    // Sample
+    // (n is -1, p is +1)
+    // ((clipping isn't a problem b/c of wrapping))
+    vec4 node_n0 = texture2D(uTex, vST + vec2(-1.0 / uTexWidth, 0.0));
+    vec4 node_0n = texture2D(uTex, vST + vec2(0.0, -1.0 / uTexHeight));
+    vec4 node_00 = texture2D(uTex, vST);
+    vec4 node_p0 = texture2D(uTex, vST + vec2(1.0 / uTexWidth, 0.0));
+    vec4 node_0p = texture2D(uTex, vST + vec2(0.0, 1.0 / uTexHeight));
+
+    // (other sampled points not actually used at this point)
+    // Just increase brightness over time to test
+    // (the *0's are so I'm not yelled at for unused variables)
+    vec4 col = node_00 + node_n0 * 0 + node_0n * 0 + node_p0 * 0 + node_0p * 0;
+    float x = col.x;
+    x = min(1.0, x + uDeltaTime * BRIGHTNESS_TRANSITION_TIME);
+    col = vec4(x, x, x, 1.0);
+    gl_FragColor = col;
+}
+`;
+
+const SHADERSTR_FLUID_DRAW_FRAG = `
+varying highp vec2 vST;
+
+uniform sampler2D uTex;
+
+const mediump vec4 LOW_COL = vec4();
+const mediump vec4 HIGH_COL = vec4();
+
+void main() {
+    // DEBUG RENDERING is just interpolating between two colors while I make sure the sim itself works
+    vec4 simData =  = BASE_COL;
+    gl_FragColor = LOW_COL * (1 - simData.r) + HIGH_COL * simData.r;
 }
 `;
